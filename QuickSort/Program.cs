6a184c45d9f50cab333579a094d9b01cf4411f20
Dianitsya <dianitasyanandamasta@gmail.com>
2022-10-20 17:00:14 +0700

    add elemen untuk array

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -29,6 +29,17 @@ namespace QuickSort
                 else
                     Console.WriteLine("\nArray can have maximum 20 elements\n");
             }
+            Console.WriteLine("\n===================");
+            Console.WriteLine("Enter Array Elements");
+            Console.WriteLine("\n===================");
+
+            //get array elements
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write("<" + (i + 1) + ">");
+                string s = Console.ReadLine();
+                arr[i] = Int32.Parse(s);
+            }
         }
         static void Main(string[] args)
         {
